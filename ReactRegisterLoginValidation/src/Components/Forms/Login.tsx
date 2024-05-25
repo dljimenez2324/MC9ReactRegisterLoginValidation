13dc3ee4385a5b411442883e72fdb0d18539c49e
dljimenez2324 <dljimenez2324@gmail.com>
2024-05-25 14:05:06 -0700

    erased pre-made elements and started the boiler plates for react components

---
+++ ReactRegisterLoginValidation/src/Components/Forms/Login.tsx
@@ -0,0 +1,11 @@
+// This component will be used to Login to the site using Zod form Validation with the requirements email and password fields
+
+const Login = () => {
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default Login
---
+++ ReactRegisterLoginValidation/src/Components/Forms/Register.tsx
@@ -0,0 +1,11 @@
+// This component will be used for users to Register for the site using Zod form Validation with the requirements first and last name, email and password with confirm password fields
+
+const Register = () => {
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default Register
