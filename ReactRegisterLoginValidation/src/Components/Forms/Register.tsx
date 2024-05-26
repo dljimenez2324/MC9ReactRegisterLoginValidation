024fb32db01cc477d180d6d3b1d2fa314c47c8d7
dljimenez2324 <dljimenez2324@gmail.com>
2024-05-26 14:35:20 -0700

    added imports for zodResolver, fieldValues, useForm and z
    also created object with  string specific validations such as .toLowerCase() .trim() .min() .email

--- ReactRegisterLoginValidation/src/Components/Forms/Register.tsx
+++ ReactRegisterLoginValidation/src/Components/Forms/Register.tsx
@@ -1,9 +1,34 @@
 // This component will be used for users to Register for the site using Zod form Validation with the requirements first and last name, email and password with confirm password fields
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FieldValues, useForm } from "react-hook-form";
+import {z} from "zod";
+
 
 const Register = () => {
+
+    // to hold the registrants data, we can use schema
+    const schema = z
+    .object({
+        firstName: z.string().toLowerCase().trim().min(2),
+        lastName: z.string().toLowerCase().trim().min(2),
+        email: z.string().trim().email(),
+        password: z.string().trim().min(6),
+        password2: z.string().trim().min(6)
+    })
+    .refine((data) => data.password === data.password2, { // checking for if passwords do not match
+        message: "Passwords do not match",  // shows this message upon passwords not matching
+        path: ["password2"]  // shows the error message for password 2
+    })
+
+
   return (
     <>
+        <div className="container">
+            <h1 className="title text-center">Register New Account</h1>
+            <form>
 
+            </form>
+        </div>
     </>
   )
 }
