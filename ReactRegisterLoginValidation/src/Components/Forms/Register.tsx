5b126af309056c41c2d413cb76560948e8ae1011
dljimenez2324 <dljimenez2324@gmail.com>
2024-05-26 21:41:06 -0700

    added styling to index.css and added input fields and labels  to the return

--- ReactRegisterLoginValidation/src/Components/Forms/Register.tsx
+++ ReactRegisterLoginValidation/src/Components/Forms/Register.tsx
@@ -4,30 +4,82 @@ import { FieldValues, useForm } from "react-hook-form";
 import {z} from "zod";
 
 // NOW I NEED TO CREATE THE HELPER FUNCTION TO HANDLE THE ONSUBMIT FROM THE FORM
-// THEN i WILL NEED TO SET UP HOW THE RETURN WILL LOOK WITH ITS INPUTS AND DISPLAY FIELDS
+// THEN I WILL NEED TO SET UP HOW THE RETURN WILL LOOK WITH ITS INPUTS AND DISPLAY FIELDS
+
+// to hold the registrants data, we can use schema
+// notice that the schema is OUTSIDE of our function.
+const schema = z
+.object({
+    firstName: z.string().toLowerCase().trim().min(2, {message: "First name must be at least 2 characters..."}),
+    lastName: z.string().toLowerCase().trim().min(2, {message: "Last name must be at least 2 characters..."}),
+    email: z.string().trim().email(),
+    password: z.string().trim().min(6),
+    password2: z.string().trim().min(6)
+})
+.refine((data) => data.password === data.password2, { // checking for if passwords do not match
+    message: "Passwords do not match",  // shows this message upon passwords not matching
+    path: ["password2"]  // shows the error message for password 2
+})
+
+// our interface with zod to hold our data
+// Also notice this interface is outside our function
+type FormData = z.infer<typeof schema>
 
 const Register = () => {
 
-    // to hold the registrants data, we can use schema
-    const schema = z
-    .object({
-        firstName: z.string().toLowerCase().trim().min(2),
-        lastName: z.string().toLowerCase().trim().min(2),
-        email: z.string().trim().email(),
-        password: z.string().trim().min(6),
-        password2: z.string().trim().min(6)
-    })
-    .refine((data) => data.password === data.password2, { // checking for if passwords do not match
-        message: "Passwords do not match",  // shows this message upon passwords not matching
-        path: ["password2"]  // shows the error message for password 2
-    })
+    // in order to see the errors we destructure the formState
+    const {register, handleSubmit, formState:{errors,isValid}} = useForm<FormData>({resolver:zodResolver(schema)});
 
+    console.log(errors);
+
+    // make a helper function that we place into the onSubmit of the form tag to see our errors in the console log
+    const onHelpSubmit = (data:FieldValues) => {
+        console.log(data);
+    }
 
   return (
     <>
-        <div className="container">
-            <h1 className="title text-center">Register New Account</h1>
-            <form>
+        <div className="container m-5 p-5">
+            <div className="row">
+                <div className="col mb-4">
+                    <h1 className="title text-center">Register New Account</h1>
+                </div>
+            </div>
+            <form onSubmit={handleSubmit(onHelpSubmit)}>
+                <div className="myContainer">
+
+                    <div className="row mySpacing">
+                        <div className="col-6">
+                            {/* First name portions */}
+                            <label htmlFor="firstName" className="form-label">First Name</label>
+                            <input {...register('firstName')} id="firstName" type="text" className="form-control"/>
+                            {errors.firstName && <p className="text-danger">{errors.firstName.message}</p>}
+                        </div>
+                        <div className="col-6">
+                            {/* Last name portions */}
+                            <label htmlFor="lastName" className="form-label">Last Name</label>
+                            <input {...register('lastName')} id="lastName" type="text" className="form-control"/>
+                            {errors.lastName && <p className="text-danger">{errors.lastName.message}</p>}
+                        </div>
+
+                    </div>
+                    <div className="row mySpacing">
+                        <div className="col-6">
+                            {/* Email section */}
+                            <label htmlFor="email" className="form-label">E-mail</label>
+                            <input {...register('email')} id="email" type="email" className="form-control"/>
+                            {errors.email && <p className="text-danger">{errors.email.message}</p>}
+                        </div>
+                    </div>
+                    <div className="row mySpacing">
+                        <div className="col-6">
+                            {/* Password sections */}
+                            <label htmlFor="password" className="form-label" >Password</label>
+                            <input {...register('password')} id="password" type="password" className="form-control"/>
+                            
+                        </div>
+                    </div>
+                </div>
 
             </form>
         </div>
