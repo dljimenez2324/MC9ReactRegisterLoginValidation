d4c76fc7702929772c6a3b56802c91b92ae3c42b
dljimenez2324 <dljimenez2324@gmail.com>
2024-05-28 22:25:54 -0700

    added title to login page

--- ReactRegisterLoginValidation/src/Components/Forms/Login.tsx
+++ ReactRegisterLoginValidation/src/Components/Forms/Login.tsx
@@ -6,7 +6,7 @@ import {z} from "zod";
 const schema = z
 .object({
   email: z.string().toLowerCase().trim(),
-  password: z.string().trim().min(6)  
+  password: z.string().trim().min(6, {message: "Password must contain at least 6 characters"})  
 })
 
 // our interface with zod to hold our data
@@ -18,10 +18,19 @@ const Login = () => {
   // to see errors we destructure our formdata
   const{register, handleSubmit, formState: {errors}} = useForm<FormData>({resolver:zodResolver(schema)});
 
+  console.log(errors);
+
+  const onHelpSubmit = (data:FieldValues) => {console.log(data)}
 
   return (
     <>
-
+        <div className="row mainCont">
+             <div className="row">
+                <div className="col my-4">
+                    <h1 className="title text-center">Login</h1>
+                </div>
+            </div>
+        </div>
     </>
   )
 }
